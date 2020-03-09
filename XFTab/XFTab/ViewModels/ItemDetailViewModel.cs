e6cbb188d1e1191161b22a53bd80cdb02b64ec5f
aodegov <altecsyst@gmail.com>
2020-03-09 22:54:55 +0200

    Tabbed layout project initialization

---
+++ XFTab/XFTab/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XFTab.Models;
+
+namespace XFTab.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
