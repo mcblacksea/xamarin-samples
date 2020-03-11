29581fc28b4c9a070d5d5e6f9f976fc1b1d9cccc
aodegov <altecsyst@gmail.com>
2020-03-12 00:45:10 +0200

    Device platform determination

--- XFEmp/XFEmp/DevPage.xaml.cs
+++ XFEmp/XFEmp/DevPage.xaml.cs
@@ -17,13 +17,31 @@ namespace XFEmp
             InitializeComponent();
             slider.Value = 0.5;
 
-            // Content in code:
-            //Content = new Label()
-            //{
-            //    HorizontalOptions = LayoutOptions.Center,
-            //    VerticalOptions = LayoutOptions.Center,
-            //    Text = "Hello"
-            //};
+            // Device Platform determination
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+                case Device.Android:
+                    Padding = new Thickness(10, 20, 0, 0);
+                    break;
+                case Device.UWP:
+                    Padding = new Thickness(30, 20, 0, 0);
+                    break;
+                default:
+                    break;
+            }
+
+            /* 
+             * Content in code: 
+             *   Content = new Label()
+             *   {
+             *       HorizontalOptions = LayoutOptions.Center,
+             *       VerticalOptions = LayoutOptions.Center,
+             *       Text = "Hello"
+             *   };
+            */
         }
     }
 }
