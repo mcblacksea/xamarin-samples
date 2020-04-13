edf23cfaa03b497e96db253caba0b52c111eabcc
aodegov <a.odegov@svitla.com>
2020-04-13 16:06:22 +0300

    029: Absolute layout in code

--- XF_Mosh/XF.PCL/Pages/AbsolutePage.xaml.cs
+++ XF_Mosh/XF.PCL/Pages/AbsolutePage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace XF.PCL.Pages
@@ -15,6 +9,16 @@ namespace XF.PCL.Pages
         public AbsolutePage()
         {
             InitializeComponent();
+            var layout = new AbsoluteLayout();
+            Content = layout;
+
+            var aquaBox = new BoxView { Color = Color.Aqua };
+            layout.Children.Add(aquaBox
+                                , new Rectangle(0, 0, 1, 1)
+                                , AbsoluteLayoutFlags.All);
+
+            // AbsoluteLayout.SetLayoutBounds(aquaBox, new Rectangle(0, 0, 1, 1));
+            // AbsoluteLayout.SetLayoutFlags(aquaBox, AbsoluteLayoutFlags.HeightProportional);
         }
     }
 }
