24784c5e295f1126fd1219ab4eeaeced553fd047
aodegov <a.odegov@svitla.com>
2020-04-13 15:59:55 +0300

    028: AbsoluteLayout in XAML

--- XF_Mosh/XF.PCL/App.xaml.cs
+++ XF_Mosh/XF.PCL/App.xaml.cs
@@ -9,7 +9,7 @@ namespace XF
         {
             InitializeComponent();
 
-            MainPage = new GridPage();
+            MainPage = new AbsolutePage();
         }
 
         protected override void OnStart()
--- XF_Mosh/XF.PCL/Pages/GridPage.xaml.cs
+++ XF_Mosh/XF.PCL/Pages/GridPage.xaml.cs
@@ -10,35 +10,35 @@ namespace XF.PCL.Pages
         {
             InitializeComponent();
 
-            var grid = new Grid
-            {
-                RowSpacing = 20,
-                ColumnSpacing = 40,
-            };
+            //var grid = new Grid
+            //{
+            //    RowSpacing = 20,
+            //    ColumnSpacing = 40,
+            //};
 
-            var label = new Label { Text = "Label Demo" };
-            grid.Children.Add(label, 0, 0);
-            Grid.SetRowSpan(label, 2);
-            Grid.SetColumnSpan(label, 2);
-            Grid.SetRow(label, 0);
-            Grid.SetColumn(label, 0);
+            //var label = new Label { Text = "Label Demo" };
+            //grid.Children.Add(label, 0, 0);
+            //Grid.SetRowSpan(label, 2);
+            //Grid.SetColumnSpan(label, 2);
+            //Grid.SetRow(label, 0);
+            //Grid.SetColumn(label, 0);
 
-            grid.RowDefinitions.Add(new RowDefinition
-            {
-                Height = new GridLength(100, GridUnitType.Absolute)
-            });
+            //grid.RowDefinitions.Add(new RowDefinition
+            //{
+            //    Height = new GridLength(100, GridUnitType.Absolute)
+            //});
 
-            grid.RowDefinitions.Add(new RowDefinition
-            {
-                Height = new GridLength(100, GridUnitType.Star)
-            });
+            //grid.RowDefinitions.Add(new RowDefinition
+            //{
+            //    Height = new GridLength(100, GridUnitType.Star)
+            //});
 
-            grid.RowDefinitions.Add(new RowDefinition
-            {
-                Height = new GridLength(100, GridUnitType.Auto)
-            });
+            //grid.RowDefinitions.Add(new RowDefinition
+            //{
+            //    Height = new GridLength(100, GridUnitType.Auto)
+            //});
 
-            this.Content = grid;
+            //this.Content = grid;
         }
     }
 }
