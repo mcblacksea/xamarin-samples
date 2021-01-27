4d6413f5d3ad451b33b16bef7c04834b67be4dda
aodegov <altecsyst@gmail.com>
2021-01-27 16:29:58 +0200

    app initializer updates

--- XamarinUITestApp/UITestExample/AppInitializer.cs
+++ XamarinUITestApp/UITestExample/AppInitializer.cs
@@ -11,6 +11,7 @@ namespace UITestExample
             if (platform == Platform.Android)
             {
                 return ConfigureApp.Android.InstalledApp("com.companyname.xamarinuitestapp").StartApp();
+                // return ConfigureApp.Android.EnableLocalScreenshots().ApkFile(@"W:\proj\dev-stack\xamarin\xamarin-samples\XamarinUITestApp\XamarinUITestApp\XamarinUITestApp.Android\bin\Debug\com.companyname.xamarinuitestapp.apk").StartApp();
             }
 
             return ConfigureApp.iOS.StartApp();
