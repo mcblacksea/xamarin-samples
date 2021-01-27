521a3d3ec2c45e1b349a0b48973c18021e605406
aodegov <altecsyst@gmail.com>
2021-01-27 16:00:19 +0200

    added existing xamarin ui test into solution

---
+++ XamarinUITestApp/UITestExample/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITestExample
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.companyname.xamarinuitestapp").StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
