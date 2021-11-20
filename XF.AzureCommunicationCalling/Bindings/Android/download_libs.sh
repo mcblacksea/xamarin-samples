4d25e7b536a2c549042268a64e1f4138e89dd938
aodegov <altecsyst@gmail.com>
2021-11-20 23:23:26 +0200

    clean up

--- XF.AzureCommunicationCalling/Bindings/Android/download_libs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm */Jars/*
-
-gradle -b calling.gradle download
-gradle -b common.gradle download
-gradle -b core.gradle download
--- XF.AzureCommunicationCalling/Bindings/Xamarin.AzureCommunicationCalling.iOS/nativeLibs/fetchPods.sh
+++
@@ -1,3 +0,0 @@
-rm -rf Pods/*
-bundle exec pod install
-
