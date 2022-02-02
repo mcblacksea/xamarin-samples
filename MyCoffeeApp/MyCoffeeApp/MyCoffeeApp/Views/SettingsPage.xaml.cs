8c6ec02bf11d21b07fe53b6c22252603057dd1f2
aodegov <altecsyst@gmail.com>
2022-02-02 03:00:23 +0200

    cleanup

--- MyCoffeeApp/MyCoffeeApp/MyCoffeeApp/Views/SettingsPage.xaml.cs
+++ MyCoffeeApp/MyCoffeeApp/MyCoffeeApp/Views/SettingsPage.xaml.cs
@@ -1,12 +1,6 @@
 ﻿using MyCoffeeApp.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace MyCoffeeApp.Views
 {
