4e1f15a908ec7f4fe5f305f425fc3f24347160a8
aodegov <altecsyst@gmail.com>
2022-02-05 04:07:28 +0200

    fixes

---
+++ AvatarGroup/People/Converters/PeopleMultiBind.cs
@@ -0,0 +1,44 @@
+﻿using People.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace People.Converters
+{
+    public class PeopleMultiBind : IMultiValueConverter
+    {
+        public int NumberToShow { get; set; } = 5;
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (var value in values)
+            {
+                if (value is IEnumerable<Person> enumerable)
+                {
+                    // get the first X number of people
+                    List<object> returnList = new List<object>();
+                    returnList.AddRange(enumerable.Take(NumberToShow));
+
+                    // if there are even more people - add a counter element
+                    if (enumerable.Count() > NumberToShow)
+                    {
+                        returnList.Add(enumerable.Count() - NumberToShow);
+                    }
+
+                    return returnList;
+                }
+                else
+                    return null;
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AvatarGroup/People/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,75 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using People.Models;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace People.ViewModels
+{
+    public class PeopleViewModel : BaseViewModel
+    {
+        public ICommand AddUserCommand { get; private set; }
+        public ICommand RemoveUserCommand { get; private set; }
+
+        public ObservableCollection<Person> People { get; set; } = new ObservableCollection<Person>();
+
+        public PeopleViewModel()
+        {
+            //PeopleCount = new List<object>();
+            
+            // setup the commands
+            AddUserCommand = new Command(AddPerson);
+            RemoveUserCommand = new Command(RemovePerson);
+            CreateSampleData();
+        }
+
+        private void CreateSampleData()
+        {
+            // create 5 peeps
+            for (int i = 0; i < 5; i++)
+            {
+                AddPerson();
+            }
+        }
+
+        private void AddPerson()
+        {
+            // add a person using an Image from pravatar.cc
+            People.Add(new Person { Image = $"https://i.pravatar.cc/64?img={People.Count + 1}" });
+
+            //OnPropertyChanged(nameof(PeopleCount));
+        }
+
+        private void RemovePerson()
+        {
+            if (People.Any())
+            {
+                People.Remove(People.Last());
+
+                //OnPropertyChanged(nameof(PeopleCount));
+
+            }
+        }
+
+        //public List<object> PeopleCount
+        //{
+        //    get
+        //    {
+        //        var numberToShow = 5;
+        //        List<object> returnList = new List<object>();
+
+        //        returnList.AddRange(People.Take(numberToShow));
+
+        //        if (People.Count > numberToShow)
+        //            returnList.Add(People.Count - numberToShow);
+
+        //        return returnList;
+        //    }
+        //    set { }
+        //} 
+    }
+}
