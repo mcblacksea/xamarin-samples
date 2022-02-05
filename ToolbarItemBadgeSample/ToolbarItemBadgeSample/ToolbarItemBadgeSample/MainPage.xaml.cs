6be162456e12a75b9bd6cff81b07f063ec2984d9
aodegov <altecsyst@gmail.com>
2022-02-05 03:41:19 +0200

    updated packages, fixed issues

--- ToolbarItemBadgeSample/ToolbarItemBadgeSample/ToolbarItemBadgeSample/MainPage.xaml.cs
+++ ToolbarItemBadgeSample/ToolbarItemBadgeSample/ToolbarItemBadgeSample/MainPage.xaml.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using ToolbarItemBadgeSample.Services;
 using Xamarin.Forms;
 
 namespace ToolbarItemBadgeSample
 {
-	public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
