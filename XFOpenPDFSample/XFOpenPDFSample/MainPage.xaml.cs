957237b85b6c84cbb95a1721bac9a3c8c872ada9
aodegov <altecsyst@gmail.com>
2022-04-13 15:07:50 +0300

    added pdf project + updates

---
+++ XFOpenPDFSample/XFOpenPDFSample/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.XamarinFormsSaveOpenPDFPackage;
+using Xamarin.Forms;
+
+namespace XFOpenPDFSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            var stream = await httpClient.GetStreamAsync("https://gerald.verslu.is/subscribe.pdf");
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await stream.CopyToAsync(memoryStream);
+
+                await CrossXamarinFormsSaveOpenPDFPackage.Current.SaveAndView("myFile.pdf", "application/pdf", memoryStream, PDFOpenContext.InApp);
+            }
+        }
+    }
+}
