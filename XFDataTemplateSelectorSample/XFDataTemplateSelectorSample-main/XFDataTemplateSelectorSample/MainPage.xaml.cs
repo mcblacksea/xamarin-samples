8add7fa74fa6cd28a06f7b4617df2c1db0db6342
aodegov <altecsyst@gmail.com>
2022-04-15 14:31:38 +0300

    added DataTemplateSelector example

---
+++ XFDataTemplateSelectorSample/XFDataTemplateSelectorSample-main/XFDataTemplateSelectorSample/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFDataTemplateSelectorSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<string> SampleItems { get; set; } = new List<string>()
+        {
+            "Hey there, friend!",
+            "Did you already subscribe?",
+            "Do it now!"
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ XFDataTemplateSelectorSample/XFDataTemplateSelectorSample-main/XFDataTemplateSelectorSample/SubscribeDataTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFDataTemplateSelectorSample
+{
+    public class SubscribeDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SubscribeTemplate { get; set; }
+        public DataTemplate OtherTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item.ToString().Contains("subscribe"))
+                return SubscribeTemplate;
+
+            return OtherTemplate;
+        }
+    }
+}
