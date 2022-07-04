a93632eb59fc5b5d114144ed861c66fe5413c4bb
aodegov <altecsyst@gmail.com>
2022-07-04 19:14:42 +0300

    added xamarin drop down menu sample

---
+++ DropdownMenu/DropdownMenu/DropdownMenu/ViewModels/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using DropdownMenu.Models.DropdownModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DropdownMenu.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public DropMenuModel GenderList { get; set; }
+        public DropMenuModel LookingforList { get; set; }
+        public ICommand GenderSelectedCommand { get; set; }
+        public ICommand LookingforListCommand { get; set; }
+        public MainViewModel()
+        {
+            GenderList = new DropMenuModel
+            {
+                Id = 1,
+                HeaderName = "Gender",
+                DropMenuList = new ObservableCollection<DropMenuItemModel>
+                    {
+                        new DropMenuItemModel
+                        {
+                            Id = 1,
+                            Name = "Male",
+                            IsSelected = false,
+                        },
+                        new DropMenuItemModel
+                        {
+                            Id = 2,
+                            Name = "Female",
+                            IsSelected = false,
+                        },
+                        new DropMenuItemModel
+                        {
+                            Id = 3,
+                            Name = "Other",
+                            IsSelected = false,
+                        },
+                    }
+            };
+            LookingforList = new DropMenuModel
+            {
+                Id = 1,
+                HeaderName = "Looking for",
+                DropMenuList = new ObservableCollection<DropMenuItemModel>
+                    {
+                        new DropMenuItemModel
+                        {
+                            Id = 1,
+                            Name = "Man",
+                            IsSelected = false,
+                        },
+                        new DropMenuItemModel
+                        {
+                            Id = 2,
+                            Name = "Woman",
+                            IsSelected = false,
+                        },
+                        new DropMenuItemModel
+                        {
+                            Id = 3,
+                            Name = "Friend",
+                            IsSelected = false,
+                        },
+                    }
+            };
+            GenderSelectedCommand = new Command(GenderSelectedCommandExecute);
+            LookingforListCommand = new Command(LookingforListCommandExecute);
+        }
+
+        private void GenderSelectedCommandExecute(object obj)
+        {
+            //first we set all is selected == false then we set new value
+            //GenderList.DropMenuList.Select(x => { x.IsSelected = false; return x; }).ToList();
+
+            //if (obj is DropMenuItemModel selecteditem)
+            //    GenderList.DropMenuList.FirstOrDefault(x => x.Id == selecteditem.Id).IsSelected = true;
+        }
+        private void LookingforListCommandExecute(object obj)
+        {
+
+        }
+    }
+}
---
+++ DropdownMenu/README.md
@@ -0,0 +1,29 @@
+# DropdownMenu
+DropdownMenu - Xamarin forms (andriod & ios)Sample Control
+## OverView
+This control help you to show a dropdown menu to user in ios & andriod 
+a quick way to select one value or multi from a set. a dropdown-menu shows its currently selected value. Touching the dropdown-menu header displays a dropdown menu with all other available values, from which the user can select a new one or multi.
+## Getting Started
+The project is up on NuGet at the following URL:
+
+https://www.nuget.org/packages/Xamarin.Forms.StateSquid
+
+Install this package into your shared project. There is no need to install it in your platform specific projects. 
+
+#### One More Step:
+Simply add the images you want to show for andriod & ios project with names 
+(CheckBoxChecked-CheckBoxUnchecked-ExpanderClose-ExpanderOpen) After that you're good to go! 
+
+if you need to make it single choice just set the single choice property = true
+```    
+    SingleChoice="True"
+```
+## Screenshots
+![alt text](https://github.com/osamaelhosany/DropdownMenu/blob/master/img/Screenshot_1.png)
+![alt text](https://github.com/osamaelhosany/DropdownMenu/blob/master/img/Screenshot_2.png)
+![alt text](https://github.com/osamaelhosany/DropdownMenu/blob/master/img/Screenshot_3.png)
+![alt text](https://github.com/osamaelhosany/DropdownMenu/blob/master/img/Screenshot_4.png)
+
+## My Controls
+- [ImageEditor](https://github.com/osamaelhosany/ImageEditor-XamarinForms)
+- [ProgressSteps](https://github.com/osamaelhosany/ProgressSteps)
