98eaafb0bf6aa960cbaaee84ca92fb3c252f43b6
aodegov <altecsyst@gmail.com>
2022-07-04 19:12:44 +0300

    added images editor project

---
+++ BitooBitImageEditor-master/src/BitooBitImageEditor.IOS/Platform.cs
@@ -0,0 +1,24 @@
+﻿namespace BitooBitImageEditor.IOS
+{
+    /// <summary>
+    /// Необходим для исспользования <see cref="BitooBitImageEditor"/> на IOS
+    /// </summary>
+    public static class Platform
+    {
+        internal static bool IsInitialized { get; set; }
+
+        /// <summary>
+        /// Инициализирует <see cref="BitooBitImageEditor"/>
+        /// </summary>
+        public static void Init()
+        {
+            IsInitialized = true;
+            LinkAssemblies();
+        }
+
+        private static void LinkAssemblies()
+        {
+
+        }
+    }
+}
