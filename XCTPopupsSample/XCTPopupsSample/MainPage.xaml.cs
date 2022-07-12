f03183956892f1d7306a250c081ed3e218f64002
aodegov <altecsyst@gmail.com>
2022-07-12 19:05:54 +0300

    added Popup solution

---
+++ XCTPopupsSample/XCTPopupsSample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+namespace XCTPopupsSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.ShowPopup(new MyPopup());
+        }
+    }
+}
