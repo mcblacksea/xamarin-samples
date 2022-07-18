cbaaf2a5dbb2ef0f7e917d2dd2fcbbabef5c8181
aodegov <altecsyst@gmail.com>
2022-07-18 14:57:19 +0300

    added sample bottom drawer

---
+++ SampleBottomDrawer/README.md
@@ -0,0 +1,9 @@
+# Sample Bottom Drawer
+
+Opens from the toolbar item. It may be closed via the backdrop tap, and by sliding it closed. Sliding it a little will result in springing back open.
+
+![Android Drawer](screenshots/droid-drawer.gif)
+
+> Currently seems there's an [iOS issue](https://github.com/xamarin/Xamarin.Forms/issues/11655) with the pan gesture and the SafeArea. I overcame it by disabling SafeArea.
+
+![iOS Drawer](screenshots/ios-drawer.gif)
---
+++ SampleBottomDrawer/src/SampleBottomDrawer/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleBottomDrawer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            //MainPage = new NavigationPage(new MainPageCode());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
