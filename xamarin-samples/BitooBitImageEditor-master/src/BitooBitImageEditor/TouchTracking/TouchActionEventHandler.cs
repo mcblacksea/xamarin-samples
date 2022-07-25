e2d4cdfc0700719d5bb13d85e7444c156025210d
aodegov <altecsyst@gmail.com>
2022-07-25 15:35:07 +0300

    Delete xamarin-samples directory

--- xamarin-samples/BitooBitImageEditor-master/src/BitooBitImageEditor/TouchTracking/TouchActionEventHandler.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace BitooBitImageEditor.TouchTracking
-{
-    /// <summary>for internal use by <see cref="BitooBitImageEditor"/></summary>
-    public delegate void TouchActionEventHandler(object sender, TouchActionEventArgs args);
-}
